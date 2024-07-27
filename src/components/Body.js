c67d9c82f942ea38aa51309ec2137db34fb879d1
ravan2202 <iamvikivivek2202@gmail.com>
2024-07-27 11:39:46 +0530

    Login Page

---
+++ src/components/Body.js
@@ -0,0 +1,28 @@
+import {createBrowserRouter} from "react-router-dom"
+import Login from './Login'
+import Browser from './Browser'
+import { RouterProvider } from "react-router-dom"
+
+const Body = () => {
+
+    const appRouter = createBrowserRouter([
+        {
+            path : "/",
+            element : <Login/>
+        },
+        {
+            path : "/browser",
+            element : <Browser/>
+        }
+    ])
+
+
+
+  return (
+    <div>
+        <RouterProvider router={appRouter}/>
+    </div>
+  )
+}
+
+export default Body
