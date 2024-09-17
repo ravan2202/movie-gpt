2b5ab33b3be15998025d0ca9a1db5730543c6b94
ravan2202 <iamvikivivek2202@gmail.com>
2024-09-17 16:24:23 +0530

    play-button

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -7,7 +7,7 @@ const VideoTitle = ({title,overview}) => {
     <p className='hidden md:inline-block py-6 text-lg w-1/3 text-justify'>{overview}</p>
     <div className='md:m-0 my-2' >
   <button className='bg-white text-black p-1 px-6 text-sm md:p-2 md:px-10 md:text-xl rounded-lg hover:opacity-80'>
-    ▶️ Play
+    Play
   </button>
   <button className=' bg-gray-500 text-white p-1 px-6 text-sm md:p-2 md:px-10 md:text-xl bg-opacity-50 rounded-lg mx-3 hover:opacity-80'>
     More Info
